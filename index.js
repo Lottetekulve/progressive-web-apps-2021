e274479df7d0653353f5442937f4b6e9c6b88b61
Lottetekulve <78353514+Lottetekulve@users.noreply.github.com>
2021-03-19 13:08:30 +0100

    render detailpage

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ const app = express()
 const port = 3000
 const getData = require('./modules/api.js')
 
-const data = getData()
-
 // Tell express to use a 'static/public' folder
 // If the url matches a file it will send that file
 app.use(express.static(path.join(__dirname, "static/public")));
@@ -18,39 +16,35 @@ app.get('/', function(req, res,) {
 });
 
 
-app.get('/artobjects', function renderOverview(req, res){
-
-  getData()
+app.get('/artobjects', async function renderOverview(req, res){
+  const url = 'https://www.rijksmuseum.nl/api/nl/collection/?key=7TAeATmh&ps=200'
+  const data = await getData(url)
+  const artList = data.artObjects
+  // console.log(artList)
 
   res.render('artObjects.ejs',{
-    artList: data
+    artList: artList
   })
-
 })
 
+app.get('/artobjects/:objectNumber', async function renderDetail(req, res){
+  const url = `https://www.rijksmuseum.nl/api/nl/collection/${req.params.objectNumber}?key=7TAeATmh`
+  const data = await getData(url)
+  const artObject = data.artObject
 
-
-
-
-// Create a route for overview page
-// app.get('/artobjects', function(err, res) {
-
-//     if (err) {
-//       // We got an error
-//       res.send(err);
-//       } 
-      
-//     else {
-
-//       res.render('artObjects.ejs', {
-//         data: body
-//         });
-//       }
-// });
-
+  res.render('artObject.ejs', { artObject })
+})
 
 
 app.listen(port, function() {
   console.log(`Example app listening at http://localhost:${port}`)
 })
 
+
+
+// links: art.links.web,
+// webImage: art.webImage.url,
+// longTitle: art.longTitle,
+// principeOrFirstMaker: art.principeOrFirstMaker,
+// productionPlaces: art.productionPlaces,
+// objectNumber: art.objectNumber
